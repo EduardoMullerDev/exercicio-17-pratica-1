1ac528ebeee36c997a3a419f7b6556eb6ff2e022
DESKTOP-SBGC454\eduar <em961226@gmail.com>
2023-04-10 21:35:13 -0300

    17.3

--- exercicio17.java
+++ exercicio17.java
@@ -1,53 +1,38 @@
 import java.util.Scanner;
-public class exercicio17{
 
-public static void main(String[] args){
+public class exercicio17 {
+    public static void main(String[] args) throws Exception {
 
-    double area;
-    double quantidadelata;
-    double mistura;
-    double misturapreco;
-    double preco;
-    double precoM;
-    double quantidadelataM;
-    double resto;
-    double restoM;
+        Scanner teclado = new Scanner(System.in);
 
-    Scanner teclado = new Scanner(System.in);
+        System.out.print("Entre com a metragem a ser pintada em m2: ");
+        double metragem = teclado.nextDouble();
 
-    System.out.println("Tamamnho em metros quadrados: " );
-    area = teclado.nextDouble();
+        teclado.close();
+     
+        double rendimentoLitros = 6;
+        double precoLata = 80.00;
+        double volumeLataLitros = 18;
+        double precoGalao = 25.00;
+        double volumeGalao = 3.6;
+        double quantidadeLatas = metragem / (volumeLataLitros * rendimentoLitros);
+        double valorTotalLatas = quantidadeLatas * precoLata;
+        double quantidadeGaloes = metragem / (volumeGalao * rendimentoLitros);
+        double valorTotalGaloes = quantidadeGaloes * precoGalao;
+        double metragemMistura = metragem * 1.1;
 
-    teclado.close();
-    
-    quantidadelata = area / (6 * 18);
-    resto = area - (6 * 18 * quantidadelata);
-    
-    if (resto > 0){
-        quantidadelata += 1;
+     
+        System.out.printf("A metragem a ser pintada é de " + metragem +  " metragem");
+        System.out.println("");
+        System.out.printf("A quantidade de latas é %f e o valor total em latas é %f", quantidadeLatas, valorTotalLatas);
+        System.out.println("");
+        System.out.printf("A quantidade de galões é %f e o valor total em galões é %f", quantidadeGaloes, valorTotalGaloes);
+        System.out.println("");
+        System.out.printf("A metragem para mistura é igual a %f", metragemMistura);
+  
+  
+  
+  
     }
-
-    preco = quantidadelata * 80;
-    
-    quantidadelataM = (int) (area / (6 *3.6));
-    restoM = (int) (area - (6*3.6 *quantidadelataM));
-
-    if (restoM > 0){
-        quantidadelata += 1;
-    }
-
-    precoM = quantidadelata * 25;
-
-    mistura = quantidadelata * quantidadelataM;
-    misturapreco = ((preco + precoM)* 0.10) + preco + precoM;
-
-    System.out.println("Latas de 18 litros: ");
-    System.out.println("Preço:"+"R$"+ preco);
-    System.out.println("Latas de 3,6 litros: ");
-    System.out.println("Preço:"+"R$ "+ precoM);
-    System.out.println("Mistura das latas: "+ mistura);
-    System.out.println("Preço: "+"R$ "+ misturapreco);
-
-}
 
 }
